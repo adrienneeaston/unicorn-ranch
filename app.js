246a7328acc8e152ec5f47efdddb298ef0756c9d
Adrienne <adrienneeaston@gmail.com>
2020-08-01 01:38:19 -0700

    object can be updated but not HTML yet

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ function Unicorn(name, color, food, location) {
   herd.push(this);
 }
 
-function displayUnicorn(obj) {
+function displayUnicorn(obj, index) {
   let el = document.getElementsByTagName('ul')[0];
   let newEl = document.createElement('li');
 
@@ -25,25 +25,31 @@ function displayUnicorn(obj) {
     description.textContent = property.toUpperCase() + ': ' + obj[property];
     newEl.appendChild(description);
   };
-  
+
+  let update = document.createElement('h4');
+  update.textContent = 'Update Location';
+  newEl.appendChild(update);
+
   let inputBarn = document.createElement('input');
   let labelBarn = document.createElement('label');
   labelBarn.textContent = 'Barn';
   inputBarn.setAttribute('type', 'radio');
   inputBarn.setAttribute('name', 'location');
-  inputBarn.setAttribute('id', obj[name] + 'barn');
+  inputBarn.setAttribute('id', index + 'barn');
+  inputBarn.setAttribute('class', index.toString());
   inputBarn.setAttribute('value', 'barn');
   labelBarn.setAttribute('for', 'barn');
   newEl.appendChild(inputBarn);
   newEl.appendChild(labelBarn);
-  console.log(obj[name] + 'barn');
+  console.log(obj.name + 'barn');
 
   let inputPasture = document.createElement('input');
   let labelPasture = document.createElement('label');
   labelPasture.textContent = 'Pasture';
   inputPasture.setAttribute('type', 'radio');
   inputPasture.setAttribute('name', 'location');
-  inputPasture.setAttribute('id', obj[name] + 'pasture');
+  inputPasture.setAttribute('id', index + 'pasture');
+  inputPasture.setAttribute('class', index.toString());
   inputPasture.setAttribute('value', 'pasture');
   labelPasture.setAttribute('for', 'pasture');
   newEl.appendChild(inputPasture);
@@ -54,7 +60,8 @@ function displayUnicorn(obj) {
   labelTrail.textContent = 'Trail';
   inputTrail.setAttribute('type', 'radio');
   inputTrail.setAttribute('name', 'location');
-  inputTrail.setAttribute('id', obj[name] + 'trail');
+  inputTrail.setAttribute('id', index + 'trail');
+  inputTrail.setAttribute('class', index.toString());
   inputTrail.setAttribute('value', 'trail');
   labelTrail.setAttribute('for', 'trail');
   newEl.appendChild(inputTrail);
@@ -65,7 +72,7 @@ function displayUnicorn(obj) {
 
 function displayHerd(arr) {
   for(let i = 0; i < arr.length; i++) {
-    displayUnicorn(arr[i]);
+    displayUnicorn(arr[i], i);
   }
 }
 
@@ -73,25 +80,27 @@ let list = document.getElementsByTagName('ul')[0];
 
 function updateLocation(event) {
   event.preventDefault();
+  let el = document.getElementById(event.target.id)
+  let index = parseInt(event.target.className);
+  herd[index].location = event.target.value;
+ 
   console.log(event);
 }
 
 list.addEventListener('change', updateLocation)
 
-
-
 new Unicorn("Rainbow", "multicolored", "icecream", "pasture");
-// new Unicorn("Sparkle", "white", "steak", "trail");
-// new Unicorn("Lightning", "gray", "catfood", "barn");
-// new Unicorn("Bob", "multicolored", "icecream", "pasture");
-// new Unicorn("Starlight", "white", "steak", "trail");
-// new Unicorn("Narwal", "gray", "catfood", "barn");
-// new Unicorn("Ezio", "black", "icecream", "pasture");
-// new Unicorn("Kassandra", "white", "steak", "trail");
-// new Unicorn("Sarah", "brown", "catfood", "barn");
-// new Unicorn("Fido", "multicolored", "icecream", "pasture");
-// new Unicorn("Spike", "pink", "steak", "trail");
-// new Unicorn("Ruby", "red", "catfood", "barn");
+new Unicorn("Sparkle", "white", "steak", "trail");
+new Unicorn("Lightning", "gray", "catfood", "barn");
+new Unicorn("Bob", "multicolored", "icecream", "pasture");
+new Unicorn("Starlight", "white", "steak", "trail");
+new Unicorn("Narwal", "gray", "catfood", "barn");
+new Unicorn("Ezio", "black", "icecream", "pasture");
+new Unicorn("Kassandra", "white", "steak", "trail");
+new Unicorn("Sarah", "brown", "catfood", "barn");
+new Unicorn("Fido", "multicolored", "icecream", "pasture");
+new Unicorn("Spike", "pink", "steak", "trail");
+new Unicorn("Ruby", "red", "catfood", "barn");
 console.log(herd);
 
 displayHerd(herd);
